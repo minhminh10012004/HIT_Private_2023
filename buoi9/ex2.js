67881101757b88c8722b4c3a90eab4cb573e4810
unknown <minhminh20041001@gmail.com>
2024-01-16 19:49:12 +0700

    project

--- buoi9/ex2.js
+++ buoi9/ex2.js
@@ -1,9 +1,9 @@
-function increaseNum(num){
-    let number = (num +1).toString();
-    let digits = number.split('');
-    for(let i=0; i<digits.length; i++){
-        digits[i] = parseInt(digits[i]);
-    }
-    return digits;
+function increaseNum(num) {
+  let number = (num + 1).toString();
+  let digits = number.split("");
+  for (let i = 0; i < digits.length; i++) {
+    digits[i] = parseInt(digits[i]);
+  }
+  return digits;
 }
-console.log(increaseNum(9999))
+console.log(increaseNum(9999));
